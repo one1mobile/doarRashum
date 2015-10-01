7470d952a928fb70a81d6da2f0bd59336b8ac5b3
onedoar <andreyz@one1.co.il>
2015-10-01 11:07:34 +0300

    add script fiile

---
+++ www/js/script.js
@@ -0,0 +1,11 @@
+﻿// validate the phone number
+function checkPhoneNumber(phone) {
+    var reg = /^(05)\d{8}$/;
+    return reg.test(phone) ? true : false;
+}
+
+// validate the barcode number
+function checkBarcodeNumber(barcode) {
+    var reg = /^[A-Z]{2}\d{9}[A-Z]{2}$/;
+    return reg.test(barcode) ? true : false;
+}
